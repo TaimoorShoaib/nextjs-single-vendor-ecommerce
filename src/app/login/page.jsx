83e7b454bc93c6b5f8529ed15ac28ab88a53be6b
Tehrimkhan <tehreemkhan565@gmail.com>
2024-02-18 15:04:29 +0500

    fixed for deployment for test 2

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,21 +1,21 @@
-"use client";
-
-import React from 'react'
-import {login} from "../../ApiRequest/internalapi"
+import React from 'react';
+import { login } from "../../ApiRequest/internalapi";
 import { useRouter } from "next/navigation";
 import { useFormik } from 'formik';
-import { useState , useEffect } from 'react';
-import loginSchema from '../../Schemas/loginSchema'
+import { useState } from 'react';
+import loginSchema from '../../Schemas/loginSchema';
 import style from "./login.module.css";
 import { useDispatch } from "react-redux";
 import { setUser } from '../../lib/userSlice';
-import TextInput from "../../components/TextInput"
+import TextInput from "../../components/TextInput";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+
 const Login = () => {
   const router = useRouter();
   const dispatch = useDispatch();
   const [error, setError] = useState("");
+
   const handleLogin = async () => {
     const data = {
       email: values.email,
@@ -30,12 +30,11 @@ const Login = () => {
         email: response.data.user.email,
         username: response.data.user.username,
         auth: response.data.auth,
-        isAdmin:response.data.user.isAdmin,
-        isVerified:response.data.user.isVerified
+        isAdmin: response.data.user.isAdmin,
+        isVerified: response.data.user.isVerified
       };
       dispatch(setUser(user));
       // Redirect to home page
-      console.log(user)
       router.push("/");
     } else if (response.code === "ERR_BAD_REQUEST") {
       setError(response.response.data.message);
@@ -49,65 +48,58 @@ const Login = () => {
     },
     validationSchema: loginSchema,
   });
-  console.log(values)
-  //{props.error && <p className={style.errorMessage}>{errors.password}</p>}
+
   return (
     <div className={style.loginPageBackground}>
-    <div className={style.loginWrapper}>
-    <hr/>
-      <div className={style.loginHeader}>login</div>
-      <hr/>
-      <div className={style.emailContainer}>
-      <PersonOutlinedIcon className={style.lockIcon}/>
-      <TextInput
-        type="text"
-        value={values.email}
-        name="email"
-        onBlur={handleBlur}
-        onChange={handleChange}
-        placeholder="Enter your Email"
-        
-      />
-      </div>
-      {errors.email && touched.email && <p className={style.errorMessageTouch}>{errors.email}</p> }
-      <div className={style.emailContainer}>
-      <LockOutlinedIcon className={style.lockIcon}/>
-      <TextInput
-        type="password"
-        name="password"
-        value={values.password}
-        onBlur={handleBlur}
-        onChange={handleChange}
-        placeholder="Enter your password"
-       
-      />
-      
-
-       </div>
-       {errors.password && touched.password && <p className={style.errorMessageTouch}>{errors.password}</p> }
-      <p  className={style.forgotPassword}>forgot password</p>
-      <button
-        className={style.loginbutton}
-        onClick={handleLogin}
-        disabled={
-          !values.email || !values.password || errors.email || errors.password
-        }
-      >
-        Log in
-      </button>
-      <span>
-        Don't have a account ?{" "}
+      <div className={style.loginWrapper}>
+        <hr />
+        <div className={style.loginHeader}>login</div>
+        <hr />
+        <div className={style.emailContainer}>
+          <PersonOutlinedIcon className={style.lockIcon} />
+          <TextInput
+            type="text"
+            value={values.email}
+            name="email"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            placeholder="Enter your Email"
+          />
+        </div>
+        {errors.email && touched.email && <p className={style.errorMessageTouch}>{errors.email}</p>}
+        <div className={style.emailContainer}>
+          <LockOutlinedIcon className={style.lockIcon} />
+          <TextInput
+            type="password"
+            name="password"
+            value={values.password}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            placeholder="Enter your password"
+          />
+        </div>
+        {errors.password && touched.password && <p className={style.errorMessageTouch}>{errors.password}</p>}
+        <p className={style.forgotPassword}>forgot password</p>
         <button
-          className={style.createAccount}
-          onClick={() => router.push("/signup")}
+          className={style.loginbutton}
+          onClick={handleLogin}
+          disabled={!values.email || !values.password || errors.email || errors.password}
         >
-          Register
+          Log in
         </button>
-      </span>
-      {error != "" ? <p className={style.errorMessage}>{error}</p> : ""}
-    </div>
+        <span>
+          Don't have a account ?{" "}
+          <button
+            className={style.createAccount}
+            onClick={() => router.push("/signup")}
+          >
+            Register
+          </button>
+        </span>
+        {error !== "" && <p className={style.errorMessage}>{error}</p>}
+      </div>
     </div>
   );
 }
 
-export default Login
+export default Login;
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -2,10 +2,10 @@ import style from './TextInput.module.css'
 
 export default function TextInput(props){
     return(
-        <div>
+        
             <div className={style.inputTextWrapper}>
                 <input {...props}/>
             </div>
-        </div>
+        
     )
 }
